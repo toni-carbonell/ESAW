da5ac165345d7aa7e060b07ae01cead7d587b976
tonij <antoni.carbonell01@estudiant.upf.edu>
2022-05-20 19:14:16 +0200

    carpetas para todos

---
+++ Lab3/Pol/Lab3_template/src/main/java/models/User.java
@@ -0,0 +1,43 @@
+package models;
+
+public class User implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String user = "";
+	private String mail = "";
+	private int[] error = {0,0}; 
+	
+	/* Getters */
+	public String getUser(){
+		return user;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	
+	public int[] getError() {
+		return error;
+	}
+	
+	/*Setters*/
+	public void setUser(String user){
+		this.user = user;
+	}
+	
+	public void setMail(String mail){
+		this.mail = mail;
+
+	}
+	
+	/* Logic Functions */
+	public boolean isComplete() {
+	    return(hasValue(getUser()) &&
+	           hasValue(getMail()) );
+	}
+	
+	private boolean hasValue(String val) {
+		return((val != null) && (!val.equals("")));
+	}
+}
